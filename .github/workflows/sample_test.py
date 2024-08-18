c67208c4a3272ad61b22dd1efb2e037abbe5b401
Chaithanya Dileep THONDAMANATI <c.thondamanati@f5.com>
2024-08-18 16:26:33 +0530

    workflow added

---
+++ .github/workflows/sample_test.py
@@ -0,0 +1,36 @@
+name: "Python Basic Test"
+
+on:
+  push:
+    branches:
+      - main
+  pull_request:
+  workflow_dispatch:
+
+jobs:
+  LB_Domain_Status_Check:
+    runs-on: ubuntu-latest
+    defaults:
+      run:
+        working-directory: ./xc
+    steps:
+    # Installing python for Validation 
+    - uses: actions/checkout@v3
+    - uses: actions/setup-python@v3
+      
+    # Install the necessary Packages
+    - name: Install the necessary packages
+      run: |
+        python -m pip install requests
+        python -m pip install tqdm
+        python -m pip install art
+        
+        
+    - name: Python Security Events get
+      run: |
+        python get_security_events.py
+
+    - name: Validate Deployment  
+      run: |
+        if [ ${{ env.EXIT }} == "true" ]; then echo "application deployment unsuccessful or application not reachable"; exit 1;\
+        else echo "deployment is successfull"; fi
